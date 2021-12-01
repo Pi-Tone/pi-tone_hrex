daa02a959e8704e51d6bd441e5419ecbb97c5b2f
kforrris <276221765@qq.com>
2021-12-01 07:01:06 -0500

    -- MainGraph Mapbox Layer updated

--- app.py
+++ app.py
@@ -23,57 +23,13 @@ DATA_PATH = os.path.join(PATH, 'data\\')
 
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
-    external_stylesheets=[dbc.themes.SUPERHERO],
+    external_stylesheets=[dbc.themes.CYBORG],
     routes_pathname_prefix='/dash-hrex-pct/'
 )
 app.title = "加拿大锦程-企业坞"
 server = app.server
 
-mapbox_access_token = "pk.eyJ1Ijoia2ZvcnJpcyIsImEiOiJja2J5a2hpMXUxMHBmMnFwYzBwNDU3andjIn0.o6CXRtTETx5soEEuCQZQ1w"
-
-# Fake data for Gantt table
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-ganttchart_example = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-
-layout = dict(
-    autosize=True,
-    automargin=True,
-    margin=dict(l=30, r=30, b=20, t=40),
-    hovermode="closest",
-    plot_bgcolor="#FFFFFF",
-    paper_bgcolor="#FFFFFF",
-    legend=dict(font=dict(size=10), orientation="h"),
-    title="业务地图",
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        style="light",
-        center=dict(lon=-171.028072, lat=38.653508),
-        zoom=7,
-    ),
-)
-excel_table = pd.read_csv(DATA_PATH + 'orderhrex.csv')
-
-def gen_data_for_excel(df, max_rows=10):
-    cols = [['Shipper Name', 'Order No.'], ['Shipping Status', 'Order Date'], ['Is Paid', 'Paid Amount']]
-    for col in cols:
-        df[col[0]] = df[col].apply(lambda row: '\n'.join(row.values.astype(str)), axis=1)
-    df = df[['Shipper Name', 'Shipping Status', 'Is Paid', 'Sale Amount']]
-    return dash_table.DataTable(
-        id="table",
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict("records"),
-        style_cell={"whiteSpace": "pre-line", 'minWidth': 95, 'maxWidth': 95, 'width': 95},
-        fixed_rows={'headers': True},
-        page_action='none',
-        style_table={'height': '500px', 'overflowY': 'auto'}
-    )
-
-
+#Create HeaderSection
 def drawHeader():
     return html.Div(
         html.A(
@@ -117,6 +73,7 @@ def drawHeader():
                     dbc.Button('了解更多',href='https://www.hr-ex.com', color='success',class_name='me-md-5 col-auto mx-auto'),
                 ),]),style={'margin-bot':'30%'})
 
+
 #Create Maingraph
 mapbox_key = "pk.eyJ1Ijoia2ZvcnJpcyIsImEiOiJja2J5a2hpMXUxMHBmMnFwYzBwNDU3andjIn0.o6CXRtTETx5soEEuCQZQ1w"
 
@@ -141,6 +98,7 @@ arc_layer = pdk.Layer(
     get_target_color=GREEN_RGB,
     pickable=True,
     auto_highlight=True,
+    wrapLongitude=True,
 )
 
 view_state = pdk.ViewState(
@@ -151,14 +109,60 @@ view_state = pdk.ViewState(
 TOOLTIP_TEXT = {
     "html": "{S000} shipments <br /> View details under table"
 }
-r = pdk.Deck(arc_layer, initial_view_state=view_state, tooltip=TOOLTIP_TEXT, map_provider='mapbox')
+r = pdk.Deck(arc_layer,initial_view_state=view_state, tooltip=TOOLTIP_TEXT, map_provider='mapbox', map_style="mapbox://styles/kforris/ckwncfb0s4fwi14nxtx6or8tu")
 
 
+#Create Status_Overview_Chart
+def Status_Overview():
+    return  html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Graph(
+                    figure=px.bar(
 
-def mainGraph():
-    return ()
+                    ).update_layout(
+                        template='plotly_dark',
+                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
+                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
+                    ),
+                    config={
+                        'displayModeBar': False
+                    }
+                ),
+            ]),style={
+                'text-align': 'center',
+                'width': 'auto',
+                'color':' white',}
+        ),
+    ])
 
-#create Graphs
+#Create GanttChart
+df = pd.DataFrame([
+    dict(Task="Freight Tracking & Management", Start='2021-01-01', Finish='2021-11-12', Staff='Terry Zhang'),
+    dict(Task="Front Desk Inquiry", Start='2021-02-22', Finish='2021-12-01', Staff="Ivy"),
+    dict(Task="Website Maintenance", Start='2021-01-05', Finish='2021-05-22', Staff="Tony Wong")
+])
+ganttchart_example = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Staff")
+
+
+excel_table = pd.read_csv(DATA_PATH + 'orderhrex.csv')
+
+def gen_excel_table_chart(df, max_rows=10):
+    cols = [['Shipper Name', 'Order No.'], ['Shipping Status', 'Order Date'], ['Is Paid', 'Paid Amount']]
+    for col in cols:
+        df[col[0]] = df[col].apply(lambda row: '\n'.join(row.values.astype(str)), axis=1)
+    df = df[['Shipper Name', 'Shipping Status', 'Is Paid', 'Sale Amount']]
+    return dash_table.DataTable(
+        id="table",
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict("records"),
+        style_cell={"whiteSpace": "pre-line", 'minWidth': 95, 'maxWidth': 95, 'width': 95},
+        fixed_rows={'headers': True},
+        page_action='none',
+        style_table={'height': '500px', 'overflowY': 'auto'}
+    )
+
+#Create RiskTrend_Chart
 def drawFigure():
     return  html.Div([
         dbc.Card(
@@ -182,9 +186,6 @@ def drawFigure():
         ),
     ])
 
-#create Ganttchart
-def ganttchart_graph():
-    ()
 
 
 # Create app layout
@@ -195,7 +196,7 @@ app.layout = html.Div(
         dbc.Row([
                     dbc.Col([
                         drawHeader(),
-                    ], style={"margin-bot":"50px"},width='100%'),
+                    ], style={"margin-bot":"50px"},width='100%', md=12),
                 ], align='center', style={"margin-bot":"500px"}),
         dbc.Card(
             dbc.CardBody([
@@ -206,23 +207,23 @@ app.layout = html.Div(
                                                   id="deck-gl",
                                                   tooltip=TOOLTIP_TEXT,
                                                   mapboxKey=r.mapbox_key,
-                                                  style={"width":"73vw","height":"35vh","display":"inline"},
-                                                  ))], width=10,
-                        style={"height":"100%"}),
+                                                  style={"width":"73vw","height":"35vh","display":"inline-block"},
+                                                  ))], width=12,
+                        style={"height":"100%", "display":"inline"}, md=10, align="start"),
                     dbc.Col([
-                        drawFigure()
-                    ], width=2,style={"height": '100%'},)
-                ], align='center'),
+                        Status_Overview()
+                    ], width=12,style={"height": '100%'}, md=2, align="end")
+                ],align="center"),
                 html.Br(),
                 dbc.Row([
                     dbc.Col([
-                        dcc.Graph(figure=ganttchart_example),
-                    ], width=3),
+                        dcc.Graph(figure=ganttchart_example, config={
+                        'displayModeBar': False}),], width=12, md=3),
                     dbc.Col([
-                        html.Div(gen_data_for_excel(excel_table)),], width=7),
+                        html.Div(gen_excel_table_chart(excel_table)),], width=12, md=7),
                     dbc.Col([
                         drawFigure()
-                    ], width=2),
+                    ], width=12, md=2),
                 ], align='center'),
             ]), color='white',),
         ],))
